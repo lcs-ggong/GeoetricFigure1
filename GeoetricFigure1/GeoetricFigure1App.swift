44ff0af3f91e6fb403bce4b446d3569355470770
Grace Gong <ggong@lcs.on.ca>
2022-11-18 17:47:22 -0500

    Initial Commit

---
+++ GeoetricFigure1/GeoetricFigure1App.swift
@@ -0,0 +1,17 @@
+//
+//  GeoetricFigure1App.swift
+//  GeoetricFigure1
+//
+//  Created by Grace Gong on 2022-11-18.
+//
+
+import SwiftUI
+
+@main
+struct GeoetricFigure1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
